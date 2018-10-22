817653683be52a46f622f11fd59082d90f4a1b3a
Pio Molina <vpio@comcast.net>
2018-10-22 16:44:46 -0400

    created schema with relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_185808) do
+ActiveRecord::Schema.define(version: 2018_10_22_200316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,20 @@ ActiveRecord::Schema.define(version: 2018_10_22_185808) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subscription_line_items", force: :cascade do |t|
+    t.bigint "medication_id"
+    t.bigint "subscription_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["medication_id"], name: "index_subscription_line_items_on_medication_id"
+    t.index ["subscription_id"], name: "index_subscription_line_items_on_subscription_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "subscription_line_items", "medications"
+  add_foreign_key "subscription_line_items", "subscriptions"
 end
